54c46d7ee79e4ec6806344f738ea35106fdf2e8e
Vaishal-Raj <vaishalraj47@gmail.com>
2024-07-25 20:51:51 +0530

    latest commit 1

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,7 @@ import Discord from "next-auth/providers/discord";
 import { db } from "./server/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { users,accounts } from "./server/db/schema";
+import { eq } from "drizzle-orm";
 import { UserRole } from "./server/db/schema";
 import { getUserById } from "./server/actions";
 import { env } from "./env";
@@ -23,9 +24,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      checks:["none"]
-      
+      clientSecret: env.GOOGLE_CLIENT_SECRET
     }),
     Discord({
       clientId: env.DISCORD_CLIENT_ID,
@@ -37,10 +36,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     pkceCodeVerifier: {
       name: "next-auth.pkce.code_verifier",
       options: {
-        httpOnly: false,
+        httpOnly: true,
         sameSite: "none",
         path: "/",
-        secure: process.env.NODE_ENV === 'production'
+        secure: true,
       },
     },
   },
